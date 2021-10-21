09f880363abce870d84daece3d6cd4b99cae3373
Nurul Ibrahim <hanisibr@gmail.com>
2021-10-21 10:35:45 +0800

    add rwd home 320px

--- src/Components/Subtitle.js
+++ src/Components/Subtitle.js
@@ -4,9 +4,8 @@ const Subtitle = () => {
   return (
     <div className="subtitle">
       <div className="main-title">
-        ONE
-        <br />
-        Dictionary
+        <div>ONE</div>
+        <div>Dictionary</div>
       </div>
       <div className="sub-title">At your fingertips</div>
     </div>
